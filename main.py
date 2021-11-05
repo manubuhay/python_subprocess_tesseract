c154016d1695b07ac6644a49b5a8678d9b75a618
Manu Buhay <manubuhay@gmail.com>
2021-11-05 07:47:58 +0000

    added subprocess.run command for both linux and windows systems

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask,url_for,redirect,render_template,request,send_file
 from werkzeug.utils import secure_filename
 import subprocess
+import os
 
 app=Flask(__name__)
-app.config['UPLOAD_DIRECTORY']="uploads/"
+app.config['UPLOAD_DIRECTORY']="uploads"
 app.config['FILE_NAME']=""
-app.config['OUTPUT_DIRECTORY']="textresult/"
-app.config['EXTENSION']=".txt"
+app.config['OUTPUT_DIRECTORY']="textresult"
+# app.config['EXTENSION']=".txt"
+app.config['EXTENSION']="txt"
 
 @app.route("/",methods=["POST","GET"])
 def to_upload():
@@ -16,7 +18,8 @@ def to_upload():
             f=request.files['fileupload']
             filename=secure_filename(f.filename)
             app.config['FILE_NAME']=filename
-            f.save(app.config['UPLOAD_DIRECTORY']+filename)
+            # f.save(app.config['UPLOAD_DIRECTORY']+filename)
+            f.save(os.path.join(app.config['UPLOAD_DIRECTORY'],filename))
             return redirect(url_for("process_upload",filename=filename))
         else:
             err_msg="No file selected!"
@@ -26,7 +29,11 @@ def to_upload():
 def process_upload(filename):
     f1=open("logs/out.txt","w")
     f2=open("logs/error.txt","w")
-    out=subprocess.run([f"tesseract uploads/{filename}"+f" textresult/{filename}"],shell=True,stdout=f1,stderr=f2)
+    to_convert=os.path.join(app.config['UPLOAD_DIRECTORY'],filename)
+    convert2txt=os.path.join(app.config['OUTPUT_DIRECTORY'],filename)
+    # out=subprocess.run([f"tesseract uploads/{filename}"+f" textresult/{filename}"],shell=True,stdout=f1,stderr=f2)
+    out=subprocess.run(["tesseract %s %s"%(to_convert,convert2txt)],shell=True,stdout=f1,stderr=f2) # Works on Linux systems, comment this if you are using Windows
+    # out=subprocess.run(["tesseract",to_convert,convert2txt],shell=True,stdout=f1,stderr=f2) # Works on Windows systems, comment this if you are using Linux
     return redirect(url_for("output_file"))
 
 @app.route("/result/",methods=["GET"])
@@ -35,7 +42,8 @@ def output_file():
 
 @app.route("/download/")
 def download_file():
-    file=app.config['OUTPUT_DIRECTORY']+app.config['FILE_NAME']+app.config['EXTENSION']
+    # file=app.config['OUTPUT_DIRECTORY']+app.config['FILE_NAME']+app.config['EXTENSION']
+    file=os.path.join(app.config['OUTPUT_DIRECTORY'],"%s.%s"%(app.config['FILE_NAME'],app.config['EXTENSION']))
     return send_file(file,as_attachment=True)
 
 if __name__=="__main__":
